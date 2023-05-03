8fd2f4b7558db21aeecc0f9e860230fa37f75dfc
Adog64 <theelectriczlime@gmail.com>
2023-05-03 17:38:45 -0400

    Hopefully triangle waves?

--- TriangleWaveTesting/main.c
+++ TriangleWaveTesting/main.c
@@ -2,47 +2,50 @@
 #include <stdbool.h>
 #include "embedded_utils.h"
 
+#define SAMPLE_COUNT 16
+#define MAX_COUNT 31250
+
+int p = 0;
+int dutyCycle = 0;
+
+void initSampleTimer();
+void initTriGenTimer();
+
 void main(void) 
 {
   killWatchdogTimer();
   unlockGPIO();
-  setAsOutput(6,6);
-  clearPinValue(6,6);
+  setAsOutput(4,1);
+  clearPinValue(4,1);
 
-  // TB0CTL = TBSSEL_1 | ID_3 | MC_3;
-  // TB0CCTL0 = CCIE;
-  // __bis_SR_register(GIE);
-  //
-  // TB0CCR0 = 500;
-  //
-  int p = 0;
-  int dutyCycle = 0;
-  int MAX_P = 16;
-  int outs[512];
-  for (int i = 0; i < 256; ++i)
-    outs[i] = i;
-  for (int i = 255; i >= 0; --i)
-    outs[256 + 255 - i] = i;
+  initSampleTimer();
+  initSampleTimer();
 
-  while(true)
-  {
-    p = 0;
-    for (int i = 0; i < 512; i++)
-    {
-      if (p >= MAX_P)
-      {
-        p = 0;
-        dutyCycle = (float)outs[i] / 256 * MAX_P;
-      }
-      else
-        p++;
-          
-      if (p < dutyCycle)
-        setPinValue(6,6);
-      else
-        clearPinValue(6,6);
+  while(true) {}
+}
 
-      // __delay_cycles(10);
-    }
-  }
+void initTriGenTimer()
+{
+  TB0CTL = TBSSEL_2 | MC_3 | ID_2;    // Use 1 MHz clock divided by 4 in up/down mode
+  TB0EX0 = TBIDEX_3;                  // Divide clock by 4 again for a total 16
+  TB0CCR0 = 239;                      // Count up to 239 before counting down (generates C3)
+}
+
+void initSampleTimer()
+{
+  TB1CTL = TBSSEL_2 | MC_1 | TBIE;    // Use 1 MHz clock in up mode with interrupts
+  TB1CCR0 = 1;                        // Set maximum count to 1
+}
+
+void __atribute__ (interrupt(TIMER1_VECTOR)) TIMER1_B1_ISR (void)
+
+  p = (p >= SAMPLE_COUNT) ? 0 : p + 1;
+  
+  if (p == 0)
+    dutyCycle = 2 * TB0R * SAMPLE_COUNT / MAX_COUNT;
+
+  if (p > dutyCycle)
+    setPinValue(4,1);
+  else
+    clearPinValue(4,1);
 }
