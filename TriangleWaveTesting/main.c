21af0d2213ea4ec1204f8bc952cd5b4be578081f
Adog64 <amsharpe102@pm.me>
2023-05-04 11:02:43 -0400

    Wave testing: devolvment into chaos

--- TriangleWaveTesting/main.c
+++ TriangleWaveTesting/main.c
@@ -1,12 +1,15 @@
 #include <msp430.h>
 #include <stdbool.h>
 #include "embedded_utils.h"
+#include "freq_period.h"
 
 #define SAMPLE_COUNT 16
 #define MAX_COUNT 31250
 
 int p = 0;
 int dutyCycle = 0;
+
+int frequency;
 
 void initSampleTimer();
 void initTriGenTimer();
@@ -15,37 +18,43 @@ void main(void)
 {
   killWatchdogTimer();
   unlockGPIO();
-  setAsOutput(4,1);
-  clearPinValue(4,1);
+  setAsOutput(1,0);
+  clearPinValue(1,0);
 
   initSampleTimer();
   initSampleTimer();
-
+  
+  __bis_SR_register(GIE);
   while(true) {}
 }
 
 void initTriGenTimer()
 {
-  TB0CTL = TBSSEL_2 | MC_3 | ID_2;    // Use 1 MHz clock divided by 4 in up/down mode
-  TB0EX0 = TBIDEX_3;                  // Divide clock by 4 again for a total 16
-  TB0CCR0 = 239;                      // Count up to 239 before counting down (generates C3)
+  TB0CTL = TBSSEL__SMCLK | MC__UPDOWN | ID_2;    // Use 1 MHz clock divided by 4 in up/down mode
+  TB0EX0 = TBIDEX_3;                             // Divide clock by 4 again for a total 16
+  TB0CCR0 = f_C3_31250;                          // Count up to 239 before counting down (generates C3)
+  freq = 
 }
 
 void initSampleTimer()
 {
-  TB1CTL = TBSSEL_2 | MC_1 | TBIE;    // Use 1 MHz clock in up mode with interrupts
-  TB1CCR0 = 1;                        // Set maximum count to 1
+  TB1CTL = TBSSEL__SMCLK | MC__UP | TBIE;      // Use 1 MHz clock in up mode with interrupts
+  TB1CCR0 = 1;                                // Set maximum count to 1
+  TB1CCR1 = 1;
 }
 
-void __atribute__ (interrupt(TIMER1_VECTOR)) TIMER1_B1_ISR (void)
-
-  p = (p >= SAMPLE_COUNT) ? 0 : p + 1;
-  
-  if (p == 0)
-    dutyCycle = 2 * TB0R * SAMPLE_COUNT / MAX_COUNT;
-
-  if (p > dutyCycle)
-    setPinValue(4,1);
-  else
-    clearPinValue(4,1);
+void __attribute__ ((interrupt(TIMER1_B1_VECTOR))) TIMER1_B1_ISR (void)
+{
+      switch(p)
+      {
+        case SAMPLE_COUNT:
+          p = 0;
+          dutyCycle = TB0R * SAMPLE_COUNT * 2 * freq;
+          P1OUT |= BIT0;
+          break;
+        case dutyCycle:
+          P1OUT &= ~BIT0;
+        default:
+      }
+      p++;
 }
